986c1a019ef8c5ae69052fa8b3b1467ffac5050f
Lars Nyström <nystrom.lasse@gmail.com>
2019-11-30 19:32:32 +0100

    Add new day script

---
+++ newday.cmd
@@ -0,0 +1,7 @@
+dotnet new console -n %1
+cd tests
+dotnet new xunit -n %1tests
+cd %1tests
+dotnet add reference ../../%1/%1.csproj
+dotnet add package Shouldly --version 4.0.0-beta0002
+dotnet restore
