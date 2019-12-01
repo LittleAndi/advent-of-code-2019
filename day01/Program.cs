352a05a14029d601a054d79e00ca2ff624474ea8
Lars Nyström <nystrom.lasse@gmail.com>
2019-12-01 06:20:58 +0100

    Add (quick) solution for part 1

--- day01/Program.cs
+++ day01/Program.cs
@@ -10,7 +10,9 @@ namespace day01
         {
             var lines = File.ReadAllLines("input.txt")
                 .Where(l => !string.IsNullOrWhiteSpace(l))
+                .Select(l => Math.Floor(decimal.Parse(l)/3)-2)
                 .ToList();
+            System.Console.WriteLine(lines.Sum());
         }
     }
 }
