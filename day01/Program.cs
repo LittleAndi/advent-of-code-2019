450733ff1ea9966681e29eeabfa2e15f7f49e20d
Lars Nyström <nystrom.lasse@gmail.com>
2019-12-01 06:49:12 +0100

    Add (quick) solution for part 2

--- day01/Program.cs
+++ day01/Program.cs
@@ -12,7 +12,20 @@ namespace day01
                 .Where(l => !string.IsNullOrWhiteSpace(l))
                 .Select(l => Math.Floor(decimal.Parse(l)/3)-2)
                 .ToList();
-            System.Console.WriteLine(lines.Sum());
+            System.Console.WriteLine($"Part 1: {lines.Sum()}");
+
+            var tot = 0m;
+            foreach (var item in lines)
+            {
+                var reqFuel = item;
+                tot+=item;
+                while ((reqFuel = Math.Floor(reqFuel/3)-2) > 0)
+                {
+                    tot+=reqFuel;
+                }
+            }
+
+            System.Console.WriteLine($"Part 2: {tot}");
         }
     }
 }
