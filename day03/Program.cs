0712bdbb55d42857efa46e70c64aac81b7a6cea3
Lars Nyström <nystrom.lasse@gmail.com>
2019-12-03 07:13:12 +0100

    Add solution for part 1

--- day03/Program.cs
+++ day03/Program.cs
@@ -12,6 +12,91 @@ namespace day03
             var lines = File.ReadAllLines("input.txt")
                 .Where(l => !string.IsNullOrWhiteSpace(l))
                 .ToList();
-       }
+
+            var directions1 = lines[0].Split(',');
+            var directions2 = lines[0].Split(',');
+
+            int[,] map = new int[20000, 20000];
+
+
+            for (int j = 0; j < 2; j++)
+            {
+                int x = map.GetLength(0) / 2;
+                int y = map.GetLength(1) / 2;
+                var instructions = lines[j].Split(',');
+
+                for (int i = 0; i < instructions.Length; i++)
+                {
+                    var ins = instructions[i];
+                    var d = ins[0];
+                    var dist = int.Parse(ins.Remove(0, 1));
+
+                    switch (d)
+                    {
+                        case 'U':
+                            {
+                                for (int my = 1; my <= dist; my++)
+                                {
+                                    y -= 1;
+                                    // System.Console.WriteLine($"{x},{y}");
+                                    map[x, y] += j + 1;
+                                }
+                            }
+                            break;
+                        case 'D':
+                            {
+                                for (int my = 1; my <= dist; my++)
+                                {
+                                    y += 1;
+                                    // System.Console.WriteLine($"{x},{y}");
+                                    map[x, y] += j + 1;
+                                }
+                            }
+                            break;
+                        case 'L':
+                            {
+                                for (int mx = 1; mx <= dist; mx++)
+                                {
+                                    x -= 1;
+                                    // System.Console.WriteLine($"{x},{y}");
+                                    map[x, y] += j + 1;
+                                }
+                            }
+                            break;
+                        case 'R':
+                            {
+                                for (int mx = 1; mx <= dist; mx++)
+                                {
+                                    x += 1;
+                                    // System.Console.WriteLine($"{x},{y}");
+                                    map[x, y] += j + 1;
+                                }
+                            }
+                            break;
+                    }
+                }
+
+            }
+     
+            int closest = map.GetLength(0)+map.GetLength(1);
+
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                for (int x = 0; x < map.GetLength(0); x++)
+                {
+                    if (map[x, y] > 2) 
+                    {
+                        var dist = Math.Abs(x-map.GetLength(0)/ 2) + Math.Abs(y - map.GetLength(1) / 2);
+                        if (dist < closest)
+                        {
+                            closest = dist;
+                            System.Console.WriteLine($"{x},{y}: {closest}");
+                            
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
