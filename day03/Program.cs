f6992d319d641787ef25c039ee5543ed335692ad
Lars Nyström <nystrom.lasse@gmail.com>
2019-12-03 06:02:59 +0100

    Add day03

---
+++ day03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt")
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .ToList();
+       }
+    }
+}
